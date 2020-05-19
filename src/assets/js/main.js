18a25d2c11397f30ede156fd6555beda38d73b14
nseven07 <necipseven07@gmail.com>
2020-05-19 15:29:24 +0300

    slider responsive

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,9 @@
 $('#slider').slick({
-
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
     arrows: true,
-    autoplay: true,
+    autoplay: false,
     speed: 1000,
     autoplaySpeed: 10000
-
-
 });
